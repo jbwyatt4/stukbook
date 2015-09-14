eb13d8df584fec72a82f20c81d6a268ad8e2f287
John B. Wyatt IV <johnwyatt@jbwyatt4.com>
2015-09-13 19:24:18 -0700

    Made show look bettr and added extra user attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {registrations: 'registrations'}
+  resources :users, only: [:show, :index]
+
   get 'pages/home'
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
